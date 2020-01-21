32db093cdd22bf35d3f0c9d36bdc5e7b28eb34fc
Jimmy Wu <jimmywu0605@gmail.com>
2020-01-21 14:08:53 -0500

    add reactive messaging to orders

--- finish/order/src/main/java/io/openliberty/guides/order/OrderManager.java
+++ finish/order/src/main/java/io/openliberty/guides/order/OrderManager.java
@@ -14,6 +14,12 @@ package io.openliberty.guides.order;
 
 import io.openliberty.guides.models.Order;
 import io.openliberty.guides.models.Status;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.reactive.streams.operators.PublisherBuilder;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.reactivestreams.Publisher;
 
 import java.util.*;
 
@@ -38,4 +44,5 @@ public class OrderManager {
     public Map<String, Order> getOrders() {
         return new HashMap<>(orders);
     }
+
  }
