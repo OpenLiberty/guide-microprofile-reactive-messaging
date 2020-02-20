52d554f3ddb3a1f6d394eea2c3f5101af497c1e8
Mai Hameed <maihameed@hotmail.com>
2020-02-20 17:44:32 -0500

    modified scripts to be run from the finish/ directory

--- finish/scripts/buildImages.sh
+++ finish/scripts/buildImages.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo Building images
-docker build -t kitchen:1.0-SNAPSHOT ../kitchen/. &
-docker build -t bar:1.0-SNAPSHOT ../bar/. &
-docker build -t servingwindow:1.0-SNAPSHOT ../servingWindow/. &
-docker build -t order:1.0-SNAPSHOT ../order/. &
-docker build -t restaurantbff:1.0-SNAPSHOT ../restaurantBFF/.
+
+docker build -t kitchen:1.0-SNAPSHOT kitchen/. &
+docker build -t bar:1.0-SNAPSHOT bar/. &
+docker build -t servingwindow:1.0-SNAPSHOT servingWindow/. &
+docker build -t order:1.0-SNAPSHOT order/. &
+docker build -t restaurantbff:1.0-SNAPSHOT restaurantBFF/.
+
 wait
 echo Images building completed
