bb1cf94a1e348eebf18f6e8e2760a5c3e8347496
Priyanshi Patel <priyanshi.patel@ibm.com>
2021-05-13 09:31:43 -0400

    Update SystemReadinessCheck.java

--- start/system/src/main/java/io/openliberty/guides/system/health/SystemReadinessCheck.java
+++ start/system/src/main/java/io/openliberty/guides/system/health/SystemReadinessCheck.java
@@ -32,12 +32,13 @@ import org.eclipse.microprofile.health.Readiness;
 @ApplicationScoped
 public class SystemReadinessCheck implements HealthCheck {
 
-    private static Logger logger = Logger.getLogger(SystemReadinessCheck.class.getName());
-    
+    private static Logger logger = Logger
+                                .getLogger(SystemReadinessCheck.class.getName());
+
     @Inject
     @ConfigProperty(name = "mp.messaging.connector.liberty-kafka.bootstrap.servers")
     String kafkaServer;
-    
+
     @Override
     public HealthCheckResponse call() {
         boolean up = isReady();
@@ -48,21 +49,22 @@ public class SystemReadinessCheck implements HealthCheck {
         AdminClient adminClient = createAdminClient();
         return checkIfBarConsumerGroupRegistered(adminClient);
     }
-    
+
     private AdminClient createAdminClient() {
         Properties connectionProperties = new Properties();
         connectionProperties.put("bootstrap.servers", kafkaServer);
         AdminClient adminClient = AdminClient.create(connectionProperties);
         return adminClient;
     }
-    
+
     private boolean checkIfBarConsumerGroupRegistered(AdminClient adminClient) {
         ListTopicsResult topics = adminClient.listTopics();
         KafkaFuture<Collection<TopicListing>> topicsFuture = topics.listings();
         try {
             Collection<TopicListing> topicList = topicsFuture.get();
-            for (TopicListing t : topicList)
+            for (TopicListing t : topicList) {
                 logger.info("topic: " + t.name());
+            }
             return true;
         } catch (Exception e) {
             return false;
