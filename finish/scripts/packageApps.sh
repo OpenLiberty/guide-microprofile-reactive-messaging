01abcb0519d1c64d060c702d608cdc5f5e3e4f7a
Austin Seto <austinseto@gmail.com>
2021-05-14 09:53:48 -0400

    Update packageApps.sh
    Update Maven options

--- finish/scripts/packageApps.sh
+++ finish/scripts/packageApps.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 mvn -pl models clean install
-mvn clean package
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -q clean package
