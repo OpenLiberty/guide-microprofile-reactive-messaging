cd203a2fe43fb29f52ec0e2b232de0d37fa59561
Austin Seto <austinseto@gmail.com>
2021-05-14 10:00:09 -0400

    Update packageApps.sh

--- finish/scripts/packageApps.sh
+++ finish/scripts/packageApps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-mvn -pl models clean install
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -pl models clean install
 mvn -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
