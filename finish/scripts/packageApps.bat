e09f317fe644bb4060315c314df22e6b8ab9ef79
Austin Seto <austinseto@gmail.com>
2021-05-14 09:58:00 -0400

    Update packageApps.bat

--- finish/scripts/packageApps.bat
+++ finish/scripts/packageApps.bat
@@ -1,5 +1,8 @@
-call mvn -pl models clean install
-call mvn -Dhttp.keepAlive=false \
-         -Dmaven.wagon.http.pool=false \
-         -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+call mvn -Dhttp.keepAlive=false ^
+         -Dmaven.wagon.http.pool=false ^
+         -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 ^
+         -pl models clean install
+call mvn -Dhttp.keepAlive=false ^
+         -Dmaven.wagon.http.pool=false ^
+         -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 ^
          -q clean package
