b8114b4d6b55a3c667978f886b203a1c340178cd
Gilbert Kwan <gkwan@ca.ibm.com>
2023-02-14 17:06:02 -0500

    use testcontainer

---
+++ finish/system/src/test/java/it/io/openliberty/guides/system/LibertyContainer.java
@@ -0,0 +1,149 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+package it.io.openliberty.guides.system;
+
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Collections;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.cxf.jaxrs.client.JAXRSClientFactoryBean;
+
+// imports for a JAXRS client to simplify the code
+
+// logger imports
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// testcontainers imports
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+// simple import to build a URI/URL
+import javax.ws.rs.core.UriBuilder;
+
+
+
+public class LibertyContainer extends GenericContainer<LibertyContainer> {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(LibertyContainer.class);
+
+    private String baseURL;
+
+    private KeyStore keystore;
+    private SSLContext sslContext;
+
+    public static String getProtocol() {
+        return System.getProperty("test.protocol", "https");
+    }
+
+    public static boolean testHttps() {
+        return getProtocol().equalsIgnoreCase("https");
+    }
+
+    public LibertyContainer(final String dockerImageName) {
+        super(dockerImageName);
+        // wait for smarter planet message by default
+        waitingFor(Wait.forLogMessage("^.*CWWKF0011I.*$", 1));
+        init();
+    }
+
+    // tag::createRestClient[]
+    public <T> T createRestClient(Class<T> clazz, String applicationPath) {
+        String urlPath = getBaseURL();
+        if (applicationPath != null) {
+            urlPath += applicationPath;
+        }
+        ClientBuilder builder = ClientBuilder.newBuilder();
+        if (testHttps()) {
+            builder.sslContext(sslContext);
+            builder.trustStore(keystore);
+        }
+        Client client = builder.build();
+        WebTarget target = client.target(UriBuilder.fromPath(urlPath));
+        //return target.proxy(clazz);
+        
+        
+        JAXRSClientFactoryBean bean = new org.apache.cxf.jaxrs.client.JAXRSClientFactoryBean();
+        bean.setResourceClass(clazz);
+        bean.setAddress(applicationPath);
+        return bean.create(clazz);
+    }
+    // end::createRestClient[]
+
+    // tag::getBaseURL[]
+    public String getBaseURL() throws IllegalStateException {
+        if (baseURL != null) {
+            return baseURL;
+        }
+        if (!this.isRunning()) {
+            throw new IllegalStateException(
+                "Container must be running to determine hostname and port");
+        }
+        baseURL =  getProtocol() + "://" + this.getContainerIpAddress()
+            + ":" + this.getFirstMappedPort();
+        System.out.println("TEST: " + baseURL);
+        return baseURL;
+    }
+    // end::getBaseURL[]
+
+    private void init() {
+
+        if (!testHttps()) {
+            this.addExposedPorts(9080);
+            return;
+        }
+
+        this.addExposedPorts(9443, 9080);
+        try {
+            String keystoreFile = System.getProperty("user.dir")
+                    + "/../../finish/system/src/main"
+                    + "/liberty/config/resources/security/key.p12";
+            keystore = KeyStore.getInstance("PKCS12");
+            keystore.load(new FileInputStream(keystoreFile), "secret".toCharArray());
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(
+                                        KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(keystore, "secret".toCharArray());
+            X509TrustManager xtm = new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType)
+                    throws CertificateException { }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType)
+                    throws CertificateException { }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+            TrustManager[] tm = new TrustManager[] {
+                                    xtm
+                                };
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kmf.getKeyManagers(), tm, new SecureRandom());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- finish/system/src/test/java/it/io/openliberty/guides/system/SystemServiceIT.java
+++ finish/system/src/test/java/it/io/openliberty/guides/system/SystemServiceIT.java
@@ -15,29 +15,68 @@ package it.io.openliberty.guides.system;
 import static org.junit.Assert.assertNotNull;
 
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.Test;
-import org.microshed.testing.SharedContainerConfig;
-import org.microshed.testing.jupiter.MicroShedTest;
-import org.microshed.testing.kafka.KafkaConsumerClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import io.openliberty.guides.models.SystemLoad;
 import io.openliberty.guides.models.SystemLoad.SystemLoadDeserializer;
 
-@MicroShedTest
-@SharedContainerConfig(AppContainerConfig.class)
+@Testcontainers
 public class SystemServiceIT {
 
-    @KafkaConsumerClient(valueDeserializer = SystemLoadDeserializer.class,
-            groupId = "system-load-status",
-            topics = "system.load",
-            properties = ConsumerConfig.AUTO_OFFSET_RESET_CONFIG + "=earliest")
-    public static KafkaConsumer<String, SystemLoad> consumer;
+	private static Logger logger = LoggerFactory.getLogger(SystemServiceIT.class);
+	private static String appImageName = "liberty-deepdive-inventory:1.0-SNAPSHOT";
+	
+	public static Network network = Network.newNetwork();
+	private static String kafkaImageName = "bitnami/kafka:2";
+	
+    @Container
+    public static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse(kafkaImageName))
+        .withNetwork(network);
+    
+	
+    //@KafkaConsumerClient(valueDeserializer = SystemLoadDeserializer.class,
+    //        groupId = "system-load-status",
+    //       topics = "system.load",
+    //        properties = ConsumerConfig.AUTO_OFFSET_RESET_CONFIG + "=earliest")
+    
+        
+    public static KafkaConsumer<String, SystemLoad> consumer =
+        new KafkaConsumer<>(getKafkaConsumerProperties(),
+            new StringDeserializer(),
+            new SystemLoadDeserializer());
+    
+    @Container
+    public static LibertyContainer libertyContainer
+        = new LibertyContainer(appImageName)
+              .withNetwork(network)
+              .waitingFor(Wait.forHttp("/health/ready"))
+              .withLogConsumer(new Slf4jLogConsumer(logger));
 
+    private static Properties getKafkaConsumerProperties() {
+    	Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", "system-load-status");
+        props.setProperty("topics", "system.load");
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		return props;
+    }
+    
     @Test
     public void testCpuStatus() {
         ConsumerRecords<String, SystemLoad> records =
