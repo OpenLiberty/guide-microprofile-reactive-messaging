da6aeb70d3b12251d1536c7932501371355d301a
Morgan Chang <shin19991207@gmail.com>
2023-09-13 15:43:12 -0400

    Revert "update to epl-2.0"
    This reverts commit a9d3f175d972c96eb94cd1eda3cfd48fcefc4f0d.

--- finish/system/src/main/java/io/openliberty/guides/system/SystemService.java
+++ finish/system/src/main/java/io/openliberty/guides/system/SystemService.java
@@ -1,12 +1,13 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2022 IBM Corporation and others.
+ * Copyright (c) 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-2.0/
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * SPDX-License-Identifier: EPL-2.0
+ * Contributors:
+ *     IBM Corporation - Initial implementation
  *******************************************************************************/
 // end::copyright[]
 package io.openliberty.guides.system;
