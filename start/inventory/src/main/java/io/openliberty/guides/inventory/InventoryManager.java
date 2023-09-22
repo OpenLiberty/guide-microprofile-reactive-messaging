101f82a6dda16b47a62862304e140d8d51c7414f
Ruilin Ma <Ruilin.Ma@ibm.com>
2023-09-22 16:35:18 -0400

    license update

--- start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
@@ -39,16 +39,7 @@ public class InventoryManager {
         if (p.isPresent()) {
             if (p.get().getProperty(hostname) == null && hostname != null) {
                 p.get().put("systemLoad", systemLoad);
-            }// tag::copyright[]
-/*******************************************************************************
- * Copyright (c) 2020, 2021 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *******************************************************************************/
+            }
         }
     }
 
