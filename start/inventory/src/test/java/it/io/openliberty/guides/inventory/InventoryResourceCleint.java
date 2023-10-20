d93555ac5815a3808cca5c2112447b68b178d6ff
Ruilin Ma <Ruilin.Ma@ibm.com>
2023-10-20 17:17:35 -0400

    test container issue fixed

--- start/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryResourceCleint.java
+++ start/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryResourceCleint.java
@@ -20,12 +20,9 @@ import jakarta.ws.rs.core.Response;
 
 @Path("/inventory")
 public interface InventoryResourceCleint {
-
-    // tag::getSystem[]
     @GET
     @Path("/systems")
     @Produces(MediaType.APPLICATION_JSON)
     Response getSystem(
         @HeaderParam("Authorization") String authHeader);
-    // end::getSystem[]
 }
