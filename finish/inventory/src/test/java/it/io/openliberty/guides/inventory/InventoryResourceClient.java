760fcd1dfc0b1d8822da4139247edba25bbe1542
Morgan Chang <shin19991207@gmail.com>
2024-04-03 11:49:35 -0400

    Update to MP61 and JEE10 (#156)
    * update finish directory to mp6/ee10
    
    * update to mpReactiveMessaging-3.0
    
    * Fix Kafka consumer stops committing offsets issue and update bitnami kafka image
    
    * update dependencies version
    
    * remove daily build
    
    * update to epl-2.0
    
    * Fix superlinter
    
    * Revert "update to epl-2.0"
    
    This reverts commit a9d3f175d972c96eb94cd1eda3cfd48fcefc4f0d.
    
    * update to epl-2.0
    
    * Fix superlinter
    
    * Fix superlinter
    
    * start directory update
    
    * license update
    
    * license update
    
    * lint correction
    
    * lint correction
    
    * lint correction
    
    * slf4j dependencies update
    
    * systemIT test update with Kafka container test
    
    * update start directory system test
    
    * update lint for system test
    
    * lint correction
    
    * fixed inventory test won't start issue
    
    * fixed inventory test won't start issue
    
    * InventoryService test added
    
    * lint correction
    
    * tag/comment added to the tests
    
    * Inventory pom update
    
    * System pom update
    
    * Address Gilbert's comment
    
    * Readme update
    
    * adjust stopContainers sequence to avoid potential issues & address Gilbert's comment
    
    * container test issue fixed
    
    * tag remove & interface update
    
    * reset system added & remove auto.commit
    
    * update default.http.port to http.port
    
    * Typo error fixed
    
    * update index.html
    
    * reverse previous change
    
    * update to latest dependencies/plugins
    
    * reverse unintended changes
    
    * reverse unintended changes
    
    * reverse unintended changes
    
    * devc & dependencies update
    
    * update bootstrap server
    
    * update /start index.html & dependencies update
    
    * Remove redundant line of code.
    
    * dependencies & logging method update
    
    * license update
    
    * Fix superlinter error
    
    * Update year and tags
    
    * update tag
    
    * update versions
    
    * update versions
    
    * update license year
    
    * polish code
    
    * polish the test code
    
    * add github action to check version
    
    * add github action
    
    * Update README.adoc
    
    remove mentioning 'zookeeper'
    
    * remove no use yml
    
    * polish the code
    
    * start kafka container first
    
    * update resteasy
    
    * update models/pom.xml to use mp 6.1
    
    ---------
    
    Co-authored-by: Ruilin Ma <Ruilin.Ma@ibm.com>
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>
    Co-authored-by: Ruilin Ma <this.is.millerma@gmail.com>
    Co-authored-by: Ruilin-Ma <71634749+Ruilin-Ma@users.noreply.github.com>

---
+++ finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryResourceClient.java
@@ -0,0 +1,33 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+// end::copyright[]
+package it.io.openliberty.guides.inventory;
+
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/inventory")
+public interface InventoryResourceClient {
+
+    @GET
+    @Path("/systems")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response getSystems();
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    Response resetSystems();
+
+}
