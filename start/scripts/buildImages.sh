e35eed3aa8b239581652cdc993410931ad2ec106
Morgan Chang <shin19991207@gmail.com>
2025-10-03 00:57:04 -0400

    update the guides to use Apache Kafka

--- start/scripts/buildImages.sh
+++ start/scripts/buildImages.sh
@@ -2,7 +2,7 @@
 
 echo Building images
 
-docker pull -q bitnamilegacy/kafka:latest
+docker pull -q apache/kafka:latest
 
 docker build -t system:1.0-SNAPSHOT system/. &
 docker build -t inventory:1.0-SNAPSHOT inventory/. &
